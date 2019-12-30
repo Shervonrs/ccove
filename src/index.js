055c7d46c4b52ee04091324331ffe505f9aa33d5
Shervonrs <47231895+Shervonrs@users.noreply.github.com>
2019-12-30 11:49:06 -0500

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import fullpage from 'fullpage.js';
 import { Map } from './maps';
 import { Swiper, Navigation, Pagination, Scrollbar } from 'swiper/js/swiper.esm.js';
 import 'swiper/css/swiper.min.css';
-
+const header = document.getElementsByClassName('header')[0];
+const activeHeader =header.offsetHeight;
+let triggeredMenu = [];
+const closeBtn = document.querySelectorAll('.menu-close-btn');
 
 document.addEventListener("DOMContentLoaded", function() {
   let mapElement = document.getElementById('map');
@@ -26,6 +29,20 @@ document.addEventListener("DOMContentLoaded", function() {
 //methods
 // fullpage_api.setAllowScrolling(true);
 
+// Triggers Nav-bar background once window Y coordinate exceeds the height of header
+function activateHeader() {
+  if(window.pageYOffset > activeHeader) {
+    header.firstElementChild.classList.add('active');
+  }
+  else {
+    header.firstElementChild.classList.remove('active');
+  }
+};
+
+window.addEventListener("scroll", () => {
+  activateHeader();
+});
+
 Swiper.use([Navigation, Pagination, Scrollbar]);
 
 var swiper = new Swiper('.architecture-section .swiper-section .swiper-container', {
@@ -38,7 +55,6 @@ var swiper = new Swiper('.architecture-section .swiper-section .swiper-container
 window.addEventListener('load', function() {
   const caption = document.querySelector('.swiper-caption.one .swiper-caption-wrapper .swiper-caption-container');
   caption.classList.add('active');
-
 });
 
 document.addEventListener('click', (e) => {
@@ -59,4 +75,45 @@ document.addEventListener('click', (e) => {
       }
     };
   }
-})
+});
+
+// Activates or deactivates navigation items
+document.addEventListener('click', (e) => {
+  let mouseclick = e.target;
+  const navItem = document.querySelectorAll('.nav-item');
+  const submenu = document.querySelectorAll('.nav-submenu-wrap');
+  const menuSpan1 = document.querySelector('.navigation-item .nav-item .menu-wrap .menuIcon.one');
+  const menuSpan2 = document.querySelector('.navigation-item .nav-item .menu-wrap .menuIcon.two');
+  const menuSpan3 = document.querySelector('.navigation-item .nav-item .menu-wrap .menuIcon.three');
+  let activemenu;
+  for(let i = 0; i < navItem.length; i++) {
+    if(mouseclick === navItem[i]) {
+      // Adds active class to all nav-submenu elements, clears act array
+      submenu[i].classList.add('active');
+      triggeredMenu = [];
+      if((i === 0 || i === 4) && ( triggeredMenu.length === 0)){
+        activemenu = document.querySelector('.nav-submenu-wrap.active');
+        trigger(i, submenu);
+      }
+    } // Triggers Menu Option
+    else if ((i === 2) && (mouseclick === menuSpan1  || mouseclick === menuSpan2 || mouseclick === menuSpan3)) {
+      submenu[i].classList.add('active');
+      activemenu = document.querySelector('.nav-submenu-wrap.active');
+      trigger(i,submenu);
+    }
+  };
+});
+
+// Iterates over closeBtn and removes active class from dropdown
+for (let i = 0; i < closeBtn.length; i++) {
+  closeBtn[i].addEventListener('click', (e) => {
+    let menuActive = document.querySelector('.nav-submenu-wrap.active');
+    menuActive.classList.remove('active');
+  })
+};
+
+// pushes nav-items into triggers array;
+function trigger (i , submenu) {
+  let activeMenu= submenu[i];
+  triggeredMenu.push(activeMenu);
+}
